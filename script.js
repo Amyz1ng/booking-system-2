f345ad1ebc0786b0880eca3e7367e7a06e91dbf8
Amy Sawyer <amyleighsawyer97@gmail.com>
2023-11-23 07:09:22 +0000

    frontend change

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     // This code will run after the DOM is fully loaded
-
-//     document.querySelector(".bg-color-three-reasons").addEventListener("mousemove", (e) => {
-//         const backgroundImage = document.getElementById("background-image");
-//         const container = document.querySelector(".bg-color-three-reasons");
-
-//         // Get the mouse position relative to the container
-//         const mouseX = e.clientX - container.getBoundingClientRect().left;
-//         const mouseY = e.clientY - container.getBoundingClientRect().top;
-
-//         // Calculate the image position based on the mouse position
-//         const imageX = (mouseX / container.offsetWidth - 0.5) * 30;
-//         const imageY = (mouseY / container.offsetHeight - 0.5) * 30;
-
-//         // Apply the transformation to the background image
-//         backgroundImage.style.transform = `translate(${imageX}px, ${imageY}px)`;
-//     });
-
-//     document.querySelector(".bg-color-three-reasons").addEventListener("mouseleave", () => {
-//         const backgroundImage = document.getElementById("background-image");
-
-//         // Reset the background image's position when the mouse leaves the container
-//         backgroundImage.style.transform = "translate(0, 0)";
-//     });
-
-//     console.log("mouse moved");
-// });
-
-// script.js
-
 function sendMail(contactForm) {
   emailjs.send("gmail", "Pizza-berra", {
     "from_name": contactForm.name.value,
@@ -46,7 +15,40 @@ function sendMail(contactForm) {
   return false;  // To block from loading a new page
 }
 
-function book() {
+async function checkAvailability() {
+  const requestData = {
+    date: document.getElementById('date').value,
+    time: document.getElementById('time').value,
+  };
+
+  try {
+    const response = await fetch('https://bookingsystem2-9ca46070b498.herokuapp.com/checkAvailability', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestData)
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    console.log('data', data);
+    if (!data.available) {
+      alert('The selected date and time are not available. Please choose another.');
+      return;
+    }else{
+      book();
+    }
+  } catch (error) {
+    console.error('There was a problem checking availability:', error);
+    return false;
+  }
+}
+
+async function book() {
   const data = {
     name: document.getElementById('fullname').value,
     email: document.getElementById('emailaddress').value,
@@ -71,8 +73,13 @@ function book() {
     })
     .then(data => {
       console.log('Insert operation successful:', data);
-      alert('Booking successful!123');
-      // Handle successful response here
+      document.getElementById('fullname').value = '';
+      document.getElementById('emailaddress').value = '';
+      document.getElementById('numberofpeople').value = '';
+      document.getElementById('date').value = '';
+      document.getElementById('time').value = '';
+      document.getElementById('bookinginfomation').value = '';
+      alert('Booking successful!');
     })
     .catch(error => {
       console.error('There was a problem with the fetch operation:', error);
@@ -80,19 +87,13 @@ function book() {
     });
 }
 
-function script(form) {
-  console.log("Form submitted");
-  console.log("Name:", form.elements.name.value);
-
-  return false;
-}
 
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('myForm');
 
   form.addEventListener('submit', function (event) {
     event.preventDefault();
-    book();
+    checkAvailability();
   });
 });
 
