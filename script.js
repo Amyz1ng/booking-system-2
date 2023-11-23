fce8f8d26f220e32ee12d20f86040b11fdaf13b8
Amy Sawyer <amyleighsawyer97@gmail.com>
2023-11-23 17:14:20 +0000

    Applied changes to url

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ async function login(loginForm) {
   };
 
   try {
-    const response = await fetch('login', { // Replace with your login endpoint URL
+    const response = await fetch('https://bookingsystem2-9ca46070b498.herokuapp.com/login', { // Replace with your login endpoint URL
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
